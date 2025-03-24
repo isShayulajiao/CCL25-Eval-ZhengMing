dff26f1f6b2add89ebd71f9f0c3a60c759a48a35
isShayulajiao <82657750+isShayulajiao@users.noreply.github.com>
2025-03-24 14:46:29 +0800

    Update setup.py

--- src/literature-evaluation/setup.py
+++ src/literature-evaluation/setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     ],
     python_requires=">=3.9",
     install_requires=[
-        "datasets>=2.0.0",
+        datasets==2.19.1",
         "einops",
         "jsonlines",
         "numexpr",
